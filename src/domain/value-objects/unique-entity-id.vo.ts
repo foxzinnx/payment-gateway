da22998986c7cccc77442eb0e4a417bf77d40528
foxz1n <bryangomes16624@gmail.com>
2026-03-05 19:11:17 -0300

    feat: initial commit

---
+++ src/domain/value-objects/unique-entity-id.vo.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 ,validate } from "uuid";
+
+export class UniqueEntityId {
+    private readonly _value: string;
+
+    constructor(id?: string){
+        if(id && !validate(id)){
+            throw new Error(`Invalid UUID: ${id}`);
+        }
+
+        this._value = id ?? uuidv4();
+    }
+
+    get value(): string {
+        return this._value
+    }
+
+    equals(other: UniqueEntityId): boolean {
+        return this._value === other._value;
+    }
+
+    toString(): string {
+        return this._value
+    }
+}
