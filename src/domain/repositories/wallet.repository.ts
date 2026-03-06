ffb2da7ac374b1a2d9b6a24bf0034d18c49df18b
foxz1n <bryangomes16624@gmail.com>
2026-03-06 20:17:52 -0300

    feat: add IWalletRepository

---
+++ src/domain/repositories/wallet.repository.ts
@@ -0,0 +1,9 @@
+import type { Wallet } from "../entities/wallet.entity.js";
+import type { UniqueEntityId } from "../value-objects/unique-entity-id.vo.js";
+
+export interface IWalletRepository {
+    findById(id: UniqueEntityId): Promise<Wallet | null>;
+    findByOwnerId(ownerId: UniqueEntityId): Promise<Wallet | null>;
+    save(wallet: Wallet): Promise<void>;
+    update(wallet: Wallet): Promise<void>;
+}
