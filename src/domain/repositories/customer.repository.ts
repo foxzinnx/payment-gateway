d9b3a8531d4531815177b01049efbd25b319ec66
foxz1n <bryangomes16624@gmail.com>
2026-03-12 22:09:54 -0300

    feat: ICustomerRepository

---
+++ src/domain/repositories/customer.repository.ts
@@ -0,0 +1,10 @@
+import type { Customer } from "../entities/customer.entity.js";
+import type { UniqueEntityId } from "../value-objects/unique-entity-id.vo.js";
+
+export interface ICustomerRepository {
+    findById(id: UniqueEntityId): Promise<Customer | null>;
+    findByEmail(email: string): Promise<Customer | null>;
+    findByCpf(cpf: string): Promise<Customer | null>;
+    save(customer: Customer): Promise<void>;
+    update(customer: Customer): Promise<void>;
+}
