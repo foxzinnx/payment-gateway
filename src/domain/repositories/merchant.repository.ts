6c712652f9ea9a42a213a7c503249568e034a191
foxz1n <bryangomes16624@gmail.com>
2026-03-12 22:15:17 -0300

    feat: IMerchantRepository

---
+++ src/domain/repositories/merchant.repository.ts
@@ -0,0 +1,10 @@
+import type { Merchant } from "../entities/merchant.entity.js";
+import type { UniqueEntityId } from "../value-objects/unique-entity-id.vo.js";
+
+export interface IMerchantRepository {
+    findById(id: UniqueEntityId): Promise<Merchant | null>;
+    findByEmail(email: string): Promise<Merchant | null>;
+    findByCnpj(cnpj: string): Promise<Merchant | null>;
+    save(merchant: Merchant): Promise<void>;
+    update(merchant: Merchant): Promise<void>;
+}
