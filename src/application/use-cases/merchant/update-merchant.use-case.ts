3b445fe0c05ef8ee21981531d421a0d11fed2f13
foxz1n <bryangomes16624@gmail.com>
2026-03-14 16:08:54 -0300

    feat: UpdateMerchantUseCase

---
+++ src/application/use-cases/merchant/update-merchant.use-case.ts
@@ -0,0 +1,49 @@
+import type { MerchantOutputDTO, UpdateMerchantInputDTO } from "@/application/dtos/merchant.dto.js";
+import type { Merchant } from "@/domain/entities/merchant.entity.js";
+import { EmailAlreadyInUseError } from "@/domain/errors/email-already-in-use.error.js";
+import { NotFoundError } from "@/domain/errors/not-found.error.js";
+import type { IMerchantRepository } from "@/domain/repositories/merchant.repository.js";
+import { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
+
+export class UpdateMerchantUseCase{
+    constructor(private readonly merchantRepository: IMerchantRepository){}
+
+    async execute(id: string, input: UpdateMerchantInputDTO): Promise<MerchantOutputDTO>{
+        const merchant = await this.merchantRepository.findById(
+            new UniqueEntityId(id)
+        );
+
+        if(!merchant){
+            throw new NotFoundError('Merchant');
+        }
+
+        if(input.email && input.email !== merchant.email.value){
+            const existingByEmail = await this.merchantRepository.findByEmail(input.email);
+            if(existingByEmail){
+                throw new EmailAlreadyInUseError();
+            }
+            merchant.updateEmail(input.email);
+        }
+
+        if(input.tradeName){
+            merchant.updateTradeName(input.tradeName);
+        }
+
+        await this.merchantRepository.update(merchant);
+
+        return this.toOutput(merchant);
+    }
+
+    private toOutput(merchant: Merchant): MerchantOutputDTO {
+        return {
+            id: merchant.id.value,
+            name: merchant.name,
+            tradeName: merchant.tradeName,
+            email: merchant.email.value,
+            cnpj: merchant.cnpj.formatted,
+            status: merchant.status,
+            createdAt: merchant.createdAt,
+            updatedAt: merchant.updatedAt,
+        }
+    }
+}
