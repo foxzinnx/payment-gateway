53ff6cc271fd765dfe8984abf7c83cc18cf4a721
foxz1n <bryangomes16624@gmail.com>
2026-03-13 21:28:39 -0300

    feat: GetMerchantByIdUseCase

---
+++ src/application/use-cases/merchant/get-merchant-by-id.use-case.ts
@@ -0,0 +1,34 @@
+import type { MerchantOutputDTO } from "@/application/dtos/merchant.dto.js";
+import type { Merchant } from "@/domain/entities/merchant.entity.js";
+import { NotFoundError } from "@/domain/errors/not-found.error.js";
+import type { IMerchantRepository } from "@/domain/repositories/merchant.repository.js";
+import { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
+
+export class GetMerchantByIdUseCase {
+    constructor(private readonly merchantRepository: IMerchantRepository){}
+
+    async execute(id: string): Promise<MerchantOutputDTO>{
+        const merchant = await this.merchantRepository.findById(
+            new UniqueEntityId(id)
+        );
+
+        if(!merchant){
+            throw new NotFoundError('Merchant');
+        }
+
+        return this.toOutput(merchant);
+    }
+
+    private toOutput(merchant: Merchant): MerchantOutputDTO {
+        return {
+            id: merchant.id.value,
+            name: merchant.name,
+            tradeName: merchant.tradeName,
+            email: merchant.email.value,
+            cnpj: merchant.cnpj.formatted,
+            status: merchant.status,
+            createdAt: merchant.createdAt,
+            updatedAt: merchant.updatedAt,
+        }
+    }
+}
