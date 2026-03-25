385a10ac52a83b6a9936bee700368e6c16f13e37
foxz1n <bryangomes16624@gmail.com>
2026-03-25 20:51:35 -0300

    feat: PrismaWalletRepository

---
+++ src/infra/database/prisma/repositories/prisma-wallet.repository.ts
@@ -0,0 +1,43 @@
+import type { Wallet } from "@/domain/entities/wallet.entity.js";
+import type { IWalletRepository } from "@/domain/repositories/wallet.repository.js";
+import type { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
+import { prisma } from "../prisma.client.js";
+import { WalletMapper } from "../mappers/wallet.mapper.js";
+
+export class PrismaWalletRepository implements IWalletRepository {
+    async findById(id: UniqueEntityId): Promise<Wallet | null> {
+        const raw = await prisma.wallet.findUnique({
+            where: { id: id.value }
+        });
+
+        if(!raw) return null
+
+        return WalletMapper.toDomain(raw);
+    }
+
+    async findByOwnerId(ownerId: UniqueEntityId): Promise<Wallet | null> {
+        const raw = await prisma.wallet.findUnique({
+            where: { ownerId: ownerId.value }
+        });
+
+        if(!raw) return null
+
+        return WalletMapper.toDomain(raw);
+    }
+
+    async save(wallet: Wallet): Promise<void> {
+        const data = WalletMapper.toPrisma(wallet);
+
+        await prisma.wallet.create({ data });
+    }
+
+    async update(wallet: Wallet): Promise<void> {
+        const data = WalletMapper.toPrisma(wallet);
+
+        await prisma.wallet.update({
+            where: { id: data.id },
+            data,
+        });
+    }
+
+}
