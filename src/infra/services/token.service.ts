b64671f87f21d58249832ba244d026d47fe0031c
foxz1n <bryangomes16624@gmail.com>
2026-03-26 22:06:43 -0300

    feat: token service

---
+++ src/infra/services/token.service.ts
@@ -0,0 +1,51 @@
+import { UnauthorizedError } from '@/domain/errors/unauthorized.error.js';
+import jwt from 'jsonwebtoken';
+
+export type TokenPayload = {
+    sub: string;
+    type: 'CUSTOMER' | 'MERCHANT'
+};
+
+export class TokenService {
+    private readonly accessSecret: string;
+    private readonly refreshSecret: string;
+    private readonly accessExpiresIn: string;
+    private readonly refreshExpiresIn: string;
+
+    constructor(){
+        this.accessSecret = process.env.JWT_ACCESS_SECRET!
+        this.refreshSecret = process.env.JWT_REFRESH_SECRET!
+        this.accessExpiresIn = process.env.JWT_ACCESS_EXPIRES_IN ?? '15min'
+        this.refreshExpiresIn = process.env.JWT_REFRESH_EXPIRES_IN ?? '7d'
+    }
+
+    generateAccessToken(payload: TokenPayload): string {
+        return jwt.sign(payload, this.accessSecret, {
+            expiresIn: this.accessExpiresIn,
+        } as jwt.SignOptions)
+    }
+
+    generateRefreshToken(payload: TokenPayload): string {
+        return jwt.sign(payload, this.refreshSecret, {
+            expiresIn: this.refreshExpiresIn,
+        } as jwt.SignOptions)
+    }
+
+    verifyAccessToken(token: string): TokenPayload {
+        try {
+            return jwt.verify(token, this.accessSecret) as TokenPayload;
+        } catch (error) {
+            throw new UnauthorizedError('Invalid or expired access token');
+        }
+    }
+
+    verifyRefreshToken(token: string): TokenPayload {
+        try {
+            return jwt.verify(token, this.refreshSecret) as TokenPayload;
+        } catch (error) {
+            throw new UnauthorizedError('Invalid or expired refresh token');
+        }
+    }
+}
+
+export const tokenService = new TokenService();
