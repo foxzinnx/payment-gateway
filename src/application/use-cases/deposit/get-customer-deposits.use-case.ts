3be7aa32e324f2e73e00e180987cd91eb7620e03
foxz1n <bryangomes16624@gmail.com>
2026-07-09 18:11:27 -0300

    refactor: fix customerId type

--- src/application/use-cases/deposit/get-customer-deposits.use-case.ts
+++ src/application/use-cases/deposit/get-customer-deposits.use-case.ts
@@ -1,12 +1,14 @@
 import type { DepositOutputDTO } from "@/application/dtos/deposit.dto.js";
 import type { DepositRepository } from "@/domain/repositories/deposit.repository.js";
-import type { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
+import { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
 
 export class GetCustomerDepositsUseCase {
     constructor(private readonly depositRepository: DepositRepository){}
 
-    async execute(customerId: UniqueEntityId): Promise<DepositOutputDTO[]>{
-        const deposits = await this.depositRepository.findAllByCustomerId(customerId);
+    async execute(customerId: string): Promise<DepositOutputDTO[]>{
+        const customerIdVO = new UniqueEntityId(customerId);
+
+        const deposits = await this.depositRepository.findAllByCustomerId(customerIdVO);
 
         return deposits.map((deposit) => deposit.toOutputDTO());
     }
