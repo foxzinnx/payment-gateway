feb8e7bcd486292deefc5bd1e4ea6cc3a1db8fe4
foxz1n <bryangomes16624@gmail.com>
2026-07-09 17:56:38 -0300

    feat(unit-of-work): add deposit unit-of-work interface

--- src/domain/repositories/unit-of-work.ts
+++ src/domain/repositories/unit-of-work.ts
@@ -1,6 +1,14 @@
+import type { Deposit } from "../entities/deposit.entity.js"
 import type { PaymentLink } from "../entities/payment-link.entity.js"
 import type { Transaction } from "../entities/transaction.entity.js"
 import type { Wallet } from "../entities/wallet.entity.js"
+
+export interface DepositUnitOfWork {
+    execute(operations: {
+        deposit: Deposit
+        wallet: Wallet
+    }): Promise<void>;
+}
 
 export interface PaymentUnitOfWork {
     execute(operations: {
