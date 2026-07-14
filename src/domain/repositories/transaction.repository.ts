b40b7a9392e1f8f11b607670cc51e0807d37b2d8
foxz1n <bryangomes16624@gmail.com>
2026-07-13 22:31:35 -0300

    feat: add findAllByMerchantId method in Transaction Repository

--- src/domain/repositories/transaction.repository.ts
+++ src/domain/repositories/transaction.repository.ts
@@ -4,6 +4,7 @@ import type { UniqueEntityId } from "../value-objects/unique-entity-id.vo.js";
 export interface TransactionRepository {
     findById(id: UniqueEntityId): Promise<Transaction | null>;
     findAllByCustomerId(customerId: UniqueEntityId): Promise<Transaction[]>;
+    findAllByMerchantId(merchantId: UniqueEntityId): Promise<Transaction[]>;
     findByIdempotencyKey(key: string): Promise<Transaction | null>;
     save(transaction: Transaction): Promise<void>;
     update(transation: Transaction): Promise<void>;
