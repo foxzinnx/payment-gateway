85e59a9afad2b7de9f61b0dd1778f75eec644c34
foxz1n <bryangomes16624@gmail.com>
2026-07-23 22:34:22 -0300

    feat: add api-key mapper

---
+++ src/infra/database/prisma/mappers/api-key.mapper.ts
@@ -0,0 +1,33 @@
+import { ApiKey } from "@/domain/entities/api-key.entity.js";
+import { UniqueEntityId } from "@/domain/value-objects/unique-entity-id.vo.js";
+import type { ApiKey as PrismaApiKey } from "generated/prisma/client.js";
+
+export class ApiKeyMapper{
+    static toDomain(raw: PrismaApiKey): ApiKey {
+        return ApiKey.reconstitute(
+            {
+                name: raw.name,
+                keyHash: raw.keyHash,
+                keyPrefix: raw.keyPrefix,
+                isActive: raw.isActive,
+                lastUsedAt: raw.lastUsedAt,
+                createdAt: raw.createdAt,
+                updatedAt: raw.updatedAt
+            },
+            new UniqueEntityId(raw.id)
+        )
+    }
+
+    static toPrisma(apiKey: ApiKey){
+        return {
+            id: apiKey.id.value,
+            name: apiKey.name,
+            keyHash: apiKey.keyHash,
+            keyPrefix: apiKey.keyPrefix,
+            isActive: apiKey.isActive,
+            lastUsedAt: apiKey.lastUsedAt ?? undefined,
+            createdAt: apiKey.createdAt,
+            updatedAt: apiKey.updatedAt
+        }
+    }
+}
